afcc5b71f348c5664dd4eb6bd5dd0956686dbb1b
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2017-05-12 20:45:10 -0400

    GAC(Global_Assembly_Cache)
    go to ReadMe File, please

---
+++ GAC(Global_Assembly_Cache)/ClassLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class MYClass
+    {
+        public static string GetValue()
+        {
+            string value = "Nazar Al-Wattar";
+            return value;
+        }
+    }
+}
---
+++ GAC(Global_Assembly_Cache)/GAC(Global_Assembly_Cache)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAC_Global_Assembly_Cache_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is my GAC Application");
+            Console.ReadKey();
+        }
+    }
+}
